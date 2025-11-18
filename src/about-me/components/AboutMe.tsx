0d2a7c70099d1135a973146123f4298e7df4a9c4
JRReduta <jrreduta@yahoo.com>
2025-11-18 04:22:34 -0800

    (change) Vertical slice - About Me section

---
+++ src/about-me/components/AboutMe.tsx
@@ -0,0 +1,52 @@
+import type { JSX } from "react";
+import "../../shared/design/base.css";
+import "../design/about-me.css";
+
+export default function AboutMe(): JSX.Element {
+    return (
+        <>
+            <AboutMeHeading
+                randomMessage={"SUPPOSED TO REMOVE THIS PLACEHOLDER"}
+            />
+            {/*TODO - literally make a thing that changes random message every 15 seconds*/}
+            <PicOfMe />
+            <Intro />
+        </>
+    );
+}
+
+function AboutMeHeading({
+    randomMessage,
+}: {
+    randomMessage: string;
+}): JSX.Element {
+    return (
+        <>
+            <h1>Jerome Reduta is...</h1>
+            <h2 className="random-message">{randomMessage}</h2>
+        </>
+    );
+}
+
+function PicOfMe(): JSX.Element {
+    return (
+        <img
+            className="pic-of-me"
+            src="/src/assets/its-me.png"
+            alt="hey that's me"
+            width="400px"
+            height="400px"
+        />
+    );
+}
+
+function Intro(): JSX.Element {
+    return (
+        <div className="intro">
+            I'm a programmer from the University of San Francisco. Write code,
+            refactor it, design it, present it, document it, test it - you name
+            it, I can do it. My dream is to craft simple, beautiful, helpful
+            things.
+        </div>
+    );
+}
--- src/portfolio-page/components/PortfolioPage.tsx
+++ src/portfolio-page/components/PortfolioPage.tsx
@@ -1,5 +1,19 @@
+import AboutMe from "../../about-me/components/AboutMe.tsx";
 import "../../shared/design/base.css";
 
 export default function PortfolioPage() {
-    return <div>AAAAAAAAAAAAAAAAAH</div>;
+    /**
+     * TODO - sections:
+     * AboutMe
+     * Projects
+     * ContactMeButton
+     */
+
+    return (
+        <>
+            <AboutMe />
+            {/* <Projects />
+        <ContactMeButton /> */}
+        </>
+    );
 }
