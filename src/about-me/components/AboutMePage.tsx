1524e97476ed6202897aaabaa51baaf968a31d40
JRReduta <jrreduta@yahoo.com>
2025-11-22 02:25:28 -0800

    (change) Got css working for whole page using lobotomized owl (desktop size only - todo: mobile-size)

--- src/about-me/components/AboutMePage.tsx
+++ src/about-me/components/AboutMePage.tsx
@@ -9,8 +9,7 @@ export default function AboutMePage(): JSX.Element {
     return (
         <section id="about-me">
             <AboutMeHeading randomMessage={message} />
-            <PicOfMe />
-            <Intro />
+            <Greeting />
             <NavArrow direction="DOWN" url="/projects" message="PROJECTS" />
         </section>
     );
@@ -22,32 +21,27 @@ function AboutMeHeading({
     randomMessage: string;
 }): JSX.Element {
     return (
-        <>
+        <div className="about-me-heading">
             <h1>Jerome Reduta is...</h1>
             <h2 className="random-message">{randomMessage}</h2>
-        </>
+        </div>
     );
 }
 
-function PicOfMe(): JSX.Element {
+function Greeting(): JSX.Element {
     return (
-        <img
-            className="pic-of-me"
-            src="/src/assets/its-me.png"
-            alt="hey that's me"
-            width="400px"
-            height="400px"
-        />
-    );
-}
-
-function Intro(): JSX.Element {
-    return (
-        <div className="intro">
-            I'm a programmer from the University of San Francisco. Write code,
-            refactor it, design it, present it, document it, test it - you name
-            it, I can do it. My dream is to craft simple, beautiful, helpful
-            things.
+        <div className="greeting">
+            <img
+                className="pic-of-me"
+                src="/src/assets/its-me.png"
+                alt="hey that's me"
+            />
+            <div className="intro">
+                I'm a programmer from the University of San Francisco. Write
+                code, refactor it, design it, present it, document it, test it -
+                you name it, I can do it. My dream is to craft simple,
+                beautiful, helpful things.
+            </div>
         </div>
     );
 }
