3f1a19c98ca234501b26467fd59d8e939e87a931
JRReduta <jrreduta@yahoo.com>
2025-11-26 02:02:04 -0800

    nav-arrow tsx & css

--- src/shared/components/NavArrow.tsx
+++ src/shared/components/NavArrow.tsx
@@ -2,7 +2,37 @@
 // import ArrowSvg from "../../assets/right-arrow.svg?react";
 // import { Link } from "react-router";
 // // import "../design/base.css"; // TODO
+import type { JSX } from "react";
+import styles from "../design/NavArrow.module.css";
+import { Link } from "react-router";
+import ArrowSvg from "../../assets/right-arrow.svg?react";
+
 // // import styles from "../design/Nav-Arrow.module.css"; // TODO
+type Direction = "UP" | "RIGHT" | "DOWN" | "LEFT";
+const map: Map<Direction, string> = new Map([
+    ["UP", styles["nav_arrow__svg_up"]],
+    ["RIGHT", styles["nav_arrow__svg_right"]],
+    ["DOWN", styles["nav_arrow__svg_down"]],
+    ["LEFT", styles["nav_arrow__svg_left"]],
+]);
+interface NavArrowProps {
+    readonly direction: Direction;
+    readonly url: string;
+    readonly message: string;
+}
+export default function NavArrow({
+    direction,
+    url,
+    message,
+}: NavArrowProps): JSX.Element {
+    const arrowClass: string = map.get(direction)!;
+    return (
+        <Link className={styles["nav_arrow"]} to={url}>
+            <div className={styles["nav_arrow__message"]}>{message}</div>
+            <ArrowSvg className={arrowClass} />
+        </Link>
+    );
+}
 
 // type Direction = "UP" | "RIGHT" | "DOWN" | "LEFT";
 // interface NavArrowProps {
--- src/welcome/components/WelcomePage.tsx
+++ src/welcome/components/WelcomePage.tsx
@@ -1,17 +1,17 @@
 import type { JSX } from "react";
 import "../../shared/design/base.css";
-// import "../design/Welcome.css";
-// import NavArrow from "../../shared/components/NavArrow";
-import useRandomMessage from "../stores/UseRandomMessage";
+import styles from "../design/Welcome.module.css";
+import NavArrow from "../../shared/components/NavArrow.tsx";
+import useRandomMessage from "../stores/UseRandomMessage.tsx";
 // import navArrowStyles from "../../shared/design/Nav-Arrow.module.css"; // TODO
 
 export default function WelcomePage(): JSX.Element {
     const { message } = useRandomMessage();
     return (
-        <section id="about-me">
+        <section className={styles["welcome"]}>
             <WelcomeHeading randomMessage={message} />
             <Greeting />
-            {/* <NavArrow direction="DOWN" url="/projects" message="PROJECTS" /> */}
+            <NavArrow direction="DOWN" url="/projects" message="PROJECTS" />
         </section>
     );
 }
@@ -22,22 +22,22 @@ function WelcomeHeading({
     randomMessage: string;
 }): JSX.Element {
     return (
-        <div className="about-me-heading">
-            <h1>Jerome Reduta is...</h1>
-            <h2 className="random-message">{randomMessage}</h2>
+        <div className={styles["heading"]}>
+            <h1 className={styles["heading__i_am"]}>Jerome Reduta is...</h1>
+            <h2 className={styles["heading__status"]}>{randomMessage}</h2>
         </div>
     );
 }
 
 function Greeting(): JSX.Element {
     return (
-        <div className="greeting">
+        <div className={styles["greeting"]}>
             <img
-                className="pic-of-me"
+                className={styles["greeting__img"]}
                 src="/src/assets/its-me.png"
                 alt="hey that's me"
             />
-            <div className="intro">
+            <div className={styles["greeting__blurb"]}>
                 I'm a programmer from the University of San Francisco. Write
                 code, refactor it, design it, present it, document it, test it -
                 you name it, I can do it. My dream is to craft simple,
